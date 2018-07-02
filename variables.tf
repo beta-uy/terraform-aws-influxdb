3b267e1ebbf9e9cc4ca768e5a6e391259242a831
Carlos Vega Meyer <carlosvega09@gmail.com>
2018-07-02 16:35:32 -0300

    Removes data_nodes security group, drops default_security_groups variable due to conditionals not being supported for list values (https://github.com/hashicorp/terraform/issues/12453)

--- variables.tf
+++ variables.tf
@@ -56,11 +56,6 @@ variable "security_groups" {
     default     = []
 }
 
-variable "default_security_groups" {
-    description = "Whether default security groups should be applied"
-    default     = true
-}
-
 variable "user_data" {
     description = "User data script for all instances"
     default     = ""
